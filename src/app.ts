672902c150976496c7c0776e2856ba3134240ae1
abanobgamil2010-coder <abanobgamil2010@gmail.com>
2026-08-18 11:37:00 +0000

    Export app for Vercel Serverless compatibility

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
-import express, { type Express } from "express";
-import cors from "cors";
-import { pinoHttp } from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
+import express from 'express';
+import cors from 'cors';
+import { pinoHttp } from 'pino-http';
 
-const app: Express = express();
+const app = express();
 
-app.use(
-
-);
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(pinoHttp());
 
-app.use("/api", router);
+app.get('/', (req, res) => {
+  res.json({ message: 'Church Course System API is running successfully!' });
+});
 
 export default app;
